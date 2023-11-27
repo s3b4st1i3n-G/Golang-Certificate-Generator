4bd2591e4809990de88267f750b6442ac21b9a24
s3b4st1i3n-G <sebgrge04@gmail.com>
2023-11-27 16:34:57 +0100

    fix: edit README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Golang-Certificate-Generator
+
+This program generates a certificate which proves that "Sebastien George" finished the Go training.
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	c, err := cert.New("Golang programming", "Bob Johnson", "2023-11-27")
+	c, err := cert.New("Golang programming", "Sebastien George", "2023-11-27")
 	if err != nil {
 		fmt.Printf("Error during certificate creation: %v", err)
 		os.Exit(1)
